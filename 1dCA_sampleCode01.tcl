98449354b493267653abdefdd384ccb66ef7b866
Yuji Sode <yuji.sode@gmail.com>
2020-12-07 15:22:17 +0900

    Add files via upload

--- 1dCA_sampleCode01.tcl
+++ 1dCA_sampleCode01.tcl
@@ -5,6 +5,8 @@
 ###
 source -encoding utf-8 1dCA.tcl;
 #
+::1dCA::setRule {111=0 110=0 101=0 100=1 011=1 010=1 001=1 000=0 name=rule_30};
+#
 proc cells {} {
 	set N 40;
 	puts stdout [string map {0 _ 1 @} [::1dCA::run [1dCA_RULE] "[string repeat 0 20]1[string repeat 0 20]" $N]];
